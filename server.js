bffbbd33469eb5a16266a28cb54334dd19f4c3c5
Kenneth Swan <ken@Kenneths-MacBook-Air.local>
2019-10-16 16:07:39 -0500

    Created functional delete route

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
-const express = require('express')
+const express = require('express');
 const app = express();
-const incredibles = require('./models/incredibles.js')
+const incredibles = require('./models/incredibles.js');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 
+//index route
 app.get('/incredibles', (req, res) => {
 	res.render('index.ejs', {
 		incredibles: incredibles
 	})
 })
 
+//middleware
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(methodOverride('_method'))
 
+//post route
+// app.post('/incredibles', (req, res) => {
+// 	console.log(req.body, ' this should be the contents of the form');
+// 	if(
+// })
+//delete route
+app.delete('/incredibles/:index', (req, res) => {
+	incredibles.splice(req.params.index, 1);
+	res.redirect('/incredibles');
+})
 
 
 

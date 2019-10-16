0edebd3384b63fcab76c6d4b60fb1db06cf9d64c
Kenneth Swan <ken@Kenneths-MacBook-Air.local>
2019-10-16 15:17:41 -0500

    Finished incredibles array

---
+++ models/incredibles.js
@@ -0,0 +1,22 @@
+const crudable = [
+	{
+		name: "Mr. Incredible",
+		power: "Super Strength"
+	},
+	{
+		name: "Mrs. Incredible",
+		power: "Elastic"
+	},
+	{
+		name: "Violet",
+		power: "Invisibility"
+	},
+	{
+		name: "Dash",
+		power: "Fast"
+	},
+	{
+		name: "Jack - Jack",
+		power: "Anger"
+	}
+]
